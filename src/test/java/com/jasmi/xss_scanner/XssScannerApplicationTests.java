8b35616390ea9e6d2e0368be7bb7a64e984ffb9f
Jasmijnvg <jasmijngenesen@gmail.com>
2024-10-01 11:52:47 +0200

    first commit

---
+++ src/test/java/com/jasmi/xss_scanner/XssScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasmi.xss_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XssScannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
